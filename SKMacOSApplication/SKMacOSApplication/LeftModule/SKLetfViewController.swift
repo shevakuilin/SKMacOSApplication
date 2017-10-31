5545276af4daf96440052217b1ebb979c9b9a7a6
ShevaKuilin <xiangkuilin@xitu.io>
2017-10-31 18:55:24 +0800

    replace resource file ---> images [TIFF -> PNG]

--- SKMacOSApplication/SKMacOSApplication/LeftModule/SKLetfViewController.swift
+++ SKMacOSApplication/SKMacOSApplication/LeftModule/SKLetfViewController.swift
@@ -19,10 +19,11 @@ class SKLetfViewController: NSViewController {
     @IBOutlet weak var collectionButton: NSButton!      // 收藏
     @IBOutlet weak var auxiliaryButton: NSButton!       // 辅助功能
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         drawMask()
+        fillDefaultData()
+        setAction()
     }
     
     // MARK: 绘制
@@ -36,6 +37,33 @@ class SKLetfViewController: NSViewController {
     private func fillDefaultData() {
         headImageView.image = NSImage(named: NSImage.Name(rawValue: "myHeadImage"))
         
+        messageButton.image = NSImage(named: NSImage.Name(rawValue: "Tabbar-Chat"))                     // 未选中状态显示的图片
+        messageButton.alternateImage = NSImage(named: NSImage.Name(rawValue: "Tabbar-Chat-Selected"))   // 选中状态显示的图片
+        friendsButton.image = NSImage(named: NSImage.Name(rawValue: "Tabbar-Contacts"))
+        friendsButton.alternateImage = NSImage(named: NSImage.Name(rawValue: "Tabbar-Contacts-Selected"))
+        collectionButton.image = NSImage(named: NSImage.Name(rawValue: "Tabbar-Favorites"))
+        collectionButton.alternateImage = NSImage(named: NSImage.Name(rawValue: "Tabbar-Favorites-Selected"))
+        auxiliaryButton.image = NSImage(named: NSImage.Name(rawValue: "Tabbar-Preferences-HI"))
     }
     
+    // MARK: 设置动作
+    private func setAction() {
+        messageButton.action = #selector(clickMessageBtn(_:))
+        friendsButton.action = #selector(clickFriendsBtn(_:))
+        collectionButton.action = #selector(clickCollectionBtn(_:))
+        auxiliaryButton.action = #selector(auxiliaryBtn(_:))
+    }
+    
+    @objc private func clickMessageBtn(_ sender: NSButton) {
+        
+    }
+    
+    @objc private func clickFriendsBtn(_ sender: NSButton) {
+    }
+    
+    @objc private func clickCollectionBtn(_ sender: NSButton) {
+    }
+    
+    @objc private func auxiliaryBtn(_ sender: NSButton) {
+    }
 }

c14367b4df3bb93d69cd52f2752ec1ea3c8dbfb0
ShevaKuilin <xiangkuilin@xitu.io>
2017-11-01 21:36:38 +0800

    add input area items

---
+++ SKMacOSApplication/SKMacOSApplication/RightModule/SKRightBottomView.swift
@@ -0,0 +1,20 @@
+//
+//  SKRightBottomView.swift
+//  SKMacOSApplication
+//
+//  Created by ShevaKuilin on 2017/11/1.
+//  Copyright © 2017年 ShevaKuilin. All rights reserved.
+//
+
+import Cocoa
+
+class SKRightBottomView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        let backgroundColor:NSColor = NSColor.white
+        backgroundColor.set()
+        NSBezierPath.fill(bounds)
+    }
+    
+}
---
+++ SKMacOSApplication/SKMacOSApplication/RightModule/SKRightBottomViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SKRightBottomViewController.swift
+//  SKMacOSApplication
+//
+//  Created by ShevaKuilin on 2017/11/1.
+//  Copyright © 2017年 ShevaKuilin. All rights reserved.
+//
+
+import Cocoa
+
+class SKRightBottomViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

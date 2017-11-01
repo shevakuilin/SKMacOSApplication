34a28284e305d1eb2709dd8e1c24ea42f756e1fc
shevakuilin <shevakuilin@gmail.com>
2017-11-01 11:17:17 +0800

    change letfView backgroundColor

--- SKMacOSApplication/SKMacOSApplication/LeftModule/SKLeftView.swift
+++ SKMacOSApplication/SKMacOSApplication/LeftModule/SKLeftView.swift
@@ -12,7 +12,7 @@ class SKLeftView: NSView {
 
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
-        let backgroundColor:NSColor = NSColor.black
+        let backgroundColor:NSColor = NSColor(red: 38/255.0, green: 38/255.0, blue: 38/255.0, alpha: 1.0)
         backgroundColor.set()
         NSBezierPath.fill(bounds)
         // Drawing code here.

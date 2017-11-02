74ccb0ff017da8ca1cb2c535729fd054cec45493
ShevaKuilin <xiangkuilin@xitu.io>
2017-11-02 10:30:54 +0800

    finished inputTextView construct

--- SKMacOSApplication/SKMacOSApplication/RightModule/SKRightBottomViewController.swift
+++ SKMacOSApplication/SKMacOSApplication/RightModule/SKRightBottomViewController.swift
@@ -10,9 +10,19 @@ import Cocoa
 
 class SKRightBottomViewController: NSViewController {
 
+    @IBOutlet var inputTextView: NSTextView!    // 输入框
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        setInputTextView()
     }
     
+    private func setInputTextView() {
+        inputTextView.backgroundColor = NSColor(red: 243/255.0, green: 243/255.0, blue: 243/255.0, alpha: 1.0)
+        inputTextView.font = NSFont.systemFont(ofSize: 14)
+        let paragraphStyle:NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 4.0
+        inputTextView.defaultParagraphStyle = paragraphStyle
+        
+    }
 }
